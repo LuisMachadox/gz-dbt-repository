1cfd62ff22f80780cb8a869c1a7f17ae5cbae266
Luis Machado <luiscarlosmachadosantos@gmail.com>
2024-02-07 16:34:39 +0000

    funcion_v1

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -3,7 +3,11 @@ FROM {{ ref('stg_raw__sales') }} as sales
 JOIN {{ ref('stg_raw__product') }} as prod
 USING (products_id))
 
-select *,
+, salesprod AS (select *,
 quantity* cast (purchase_price as float64) as purchase_cost,
 round (revenue-(quantity* cast (purchase_price as float64)),2) as margin
-from sales_prod
+from sales_prod)
+
+SELECT *
+,{{margin_percent(revenue,purchase_cost)}} as margin_percent
+from salesprod
